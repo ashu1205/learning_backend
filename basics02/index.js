59e864eea675df9476589cba800f7aec11a21534
Your Name <your.email@example.com>
2024-01-29 10:53:47 +0530

    working with view engine and ejs

---
+++ basics02/index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+
+const app=express();
+
+app.listen(4000,()=>{
+    console.log('server started at port 4000');
+})
+
+// to display content on client side => 
+// so we use view engine to run "templating languages"
+// view engine => machine to run "templating languages"  like ejs ,handlabs
+app.set('view Engine','hbs' ,'ejs')
+app.set('views',"./views")
